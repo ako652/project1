027a0c393fe407ad62e8bf3d32d834216461587a
ako652 <akotabe0213@gmail.com>
2021-11-28 19:15:41 +0100

    search string
    toggle border color and search string included

--- src/components/Booklist.jsx
+++ src/components/Booklist.jsx
@@ -1,21 +1,38 @@
 import { Component } from "react";
 import Singlebooks from "./Singlebooks";
 import Container from 'react-bootstrap/Container'
-import { Row ,Col} from "react-bootstrap";
+import { Row ,Col,Form} from "react-bootstrap";
 
 
 
 class Booklist extends Component{
 
+    state = {
+        searchQuery: ''
+    }
 
     render(){
         return(
+            
             <Container>
+                <Col>
+                
+                <Form.Group controlId="formBasicEmail">
+                            <Form.Label>Search</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Search here"
+                                value={this.state.searchQuery}
+                                onChange={e => this.setState({ searchQuery: e.target.value })}
+                            />
+                        </Form.Group>
+                </Col>
+                
                 <Row>
                     
                     {
         
-                        this.props.bookpage.map(bk =>(
+                        this.props.bookpage.filter(bk=>bk.title.toLowerCase().includes(this.state.searchQuery)).map(bk =>(
                             <Col xs={3}>
                             <Singlebooks book={bk}/>
                             </Col>
--- src/components/Singlebooks.jsx
+++ src/components/Singlebooks.jsx
@@ -1,20 +1,27 @@
-
+import { Component } from 'react'
 import {Card,CardGroup} from 'react-bootstrap'
 
-const Singlebooks =(props)=>{
+class Singlebooks extends Component{
+  state={
+    selected: false
+
+  }
+
+  render(){
     return(
        
         
     
-
     
-    <CardGroup key={props.book.asin}>
-  <Card>
-    <Card.Img variant="top" src={props.book.img} />
+    <CardGroup key={this.props.book.asin}>
+  <Card onClick={()=>this.setState({selected: !this.state.selected})}
+  style={{border: this.state.selected? '3px solid red' :'none'}}
+  >
+    <Card.Img variant="top" src={this.props.book.img} />
     <Card.Body>
-      <Card.Title>{props.book.category}</Card.Title>
+      <Card.Title>{this.props.book.category}</Card.Title>
       <Card.Text>
-        {props.book.title}
+        {this.props.book.title}
       </Card.Text>
     </Card.Body>
     <Card.Footer>
@@ -24,6 +31,7 @@ const Singlebooks =(props)=>{
   
   
 </CardGroup>
+    
   
     
 
@@ -32,7 +40,7 @@ const Singlebooks =(props)=>{
 
 
 
-)
+)}
 
 
     
