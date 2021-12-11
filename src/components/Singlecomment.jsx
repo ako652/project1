0d9ae6218a5ec1ee32e2080bed0234adab315a0d
ako652 <akotabe0213@gmail.com>
2021-12-11 14:05:04 +0100

    Update Singlecomment.jsx
    delete button added

--- src/components/Singlecomment.jsx
+++ src/components/Singlecomment.jsx
@@ -1,12 +1,41 @@
 
-import { ListGroup } from "react-bootstrap"
 
+import { ListGroup ,Button} from "react-bootstrap"
 
-const Singlecomment = ({ comment }) => (
-    <ListGroup.Item>
-        {comment.comment}
-        
-    </ListGroup.Item>
-)
+const Deleteback=async(asin)=>{
+  try {
+    let response=await  fetch('https://striveschool-api.herokuapp.com/api/comments/' + asin, {
+        method: 'DELETE',
+        headers: {
+           Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGRjNjA5MmIzNTgxNzAwMTVjMjI3MGIiLCJpYXQiOjE2Mzg3MDE0MDUsImV4cCI6MTYzOTkxMTAwNX0.QIlhYQJuLxVAQ1YBmrHO4Iutg3NkyevRoh4_ZuktfaI'
+        }
+    })
+    if (response.ok){
+        alert("awesome")
+    }else{
+        console.log("error")
+    }
+  } catch (error) {
+      console.log("error")
+  }
+
+}
+
+   
+    
+   const Singlecomment=({comment})=>{
+    return(
+        <ListGroup.Item>
+    {comment.comment}
+    <Button variant="primary" style={{margin:'2px'}} onClick={()=> Deleteback(comment._id)}>delete</Button>{' '}
+</ListGroup.Item>
+    )
+   }
+    
+
+    
+
+  
+
 
 export default Singlecomment
